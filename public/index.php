365166d31b5fb26a969b1c25dc7a51911357011c
Kevin <kevinkuchar2378@gmail.com>
2016-09-20 22:37:37 -0400

    Added .sql file

--- public/index.php
+++ public/index.php
@@ -9,6 +9,18 @@ spl_autoload_register(function ($classname) {
 $settings = require __DIR__ . '/../app/settings.php';
 $app = new \Slim\App($settings);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // Register dependencies
 require __DIR__ . '/../app/dependencies.php';
 
